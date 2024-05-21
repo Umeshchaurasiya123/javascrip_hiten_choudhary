06177a267d71ce212df7ce94d52f0f2375e29c9b
Umesh <92439041+Umeshchaurasiya123@users.noreply.github.com>
2024-05-21 17:57:36 +0000

    let var const variables

--- .devcontainer/javascript/test.js
+++ .devcontainer/javascript/test.js
@@ -1,1 +1,25 @@
 console.log("Hello World");
+
+
+// var const and let keyword in javascript. 
+
+const accoundID=12345; 
+
+let accountEmail="abcd@gmail.com"
+
+var accountPassword="12345"
+
+let accountState; 
+
+accountCity="mumbai"
+
+// accondID=2  not allowed. 
+
+accountEmail="pqrs@gmail.com"
+
+accountPassword="ashwniloveumesh"
+
+accountCity="bihar"
+
+console.table([accountCity,accountState,accountPassword,accountEmail,accoundID])
+
