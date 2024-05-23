e77619e23c1e96646423570634866da43b8d0589
Umesh <92439041+Umeshchaurasiya123@users.noreply.github.com>
2024-05-23 17:19:50 +0000

    Convertion of data type

--- .devcontainer/javascript/test.js
+++ .devcontainer/javascript/test.js
@@ -23,3 +23,37 @@ accountCity="bihar"
 
 console.table([accountCity,accountState,accountPassword,accountEmail,accoundID])
 
+
+// chai and code  episoe number 5
+
+"use strict"; //  treat all the js code as newver version. 
+
+//alert(1+2+3) we are using node not browser it will give eror. as in browser javascrit run in binding with html. 
+
+// null is standalone value and undefine is not an value. 
+
+// symbol -> use for uniquness of components. 
+
+
+// object
+
+console.log(typeof null);        // object
+console.log(typeof undefined);   // undefined
+
+
+// episode number 6
+
+// this is for number function Number()
+// "33" => 33
+// "33abc"=> Nan // not an number its type is even number 
+// true=1 ; fasle =0 
+
+// this is for Boolean function Boolean()
+// 1=> true ; 0=> fasle 
+// "" => false
+// "umesh" => true
+
+// now for String function String()
+
+
+
