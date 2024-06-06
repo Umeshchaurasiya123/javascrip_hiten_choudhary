db5d257f0f6b464ef4a34a37300ed5f30aa9b914
Umesh <92439041+Umeshchaurasiya123@users.noreply.github.com>
2024-06-06 17:08:32 +0000

    iteration in javascript

---
+++ .devcontainer/javascript/iteration_break_countinue.js
@@ -0,0 +1,18 @@
+
+for (let index = 0; index < 10; index++) {
+    const element = index
+    console.log(element)
+
+}
+
+let  fuck=0;
+while (fuck<69) {
+    console.log(fuck)
+    fuck++;
+}
+
+do {
+
+
+    
+} while (condition);
